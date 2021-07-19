d5d77a940fdbd69e45c2df8f2dba536b50c842e3
Brennan Beam <brennanbeam@gmail.com>
2021-07-19 15:07:11 -0700

    add example data. read in example data

--- npy2csv.py
+++ npy2csv.py
@@ -1,2 +1,12 @@
-msg = "hello world"
-print(msg)
+import numpy 
+
+#USER SETTINGS
+#Relative Path to Test Dataset = "Resources/syn_hcstLm.npy"
+_path = "Resources/syn_hcstLm.npy"
+
+myEnsembleData = numpy.load(_path)
+print("successfully loaded the data into memory")
+
+#Commented out because this doesnt work for the 3D array.
+#numpy.savetxt('Resources/syn_hcstLm.csv', myEnsembleData, delimiter=',')
+#print("saved data as .csv")
