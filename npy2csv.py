dd21451766d5301240145010649ec57e763229de
Brennan Beam <BrennanBeam@gmail.com>
2022-06-24 09:32:03 -0700

    now loops through all the files in the directory

--- npy2csv.py
+++ npy2csv.py
@@ -1,17 +1,22 @@
 #Purpose: synthetic ensemble forecast data was provided in .npy format, as an 3D array. This script creates a csv file for each day, with all traces.
-import numpy 
+from msilib.schema import Directory
+import numpy
+import os 
 
 #USER SETTINGS
-#Relative Path to Test Dataset = "Resources/syn_hcstLm.npy"
-_path = "Resources/syn_hcstLm.npy"
+#Relative Path to Test Dataset = "Resources/"
+directory = "C:\\Users\\q0hecbbb\\Projects\\Hindcast_Revisited\\fromZach22June10"
 
-myEnsembleData = numpy.load(_path)
-print("successfully loaded the data into memory.")
-
-DayNumFileName = 0 
-for eachday in myEnsembleData:
-    numpy.savetxt('Resources/syn_hcstLm' + str(DayNumFileName) + '.csv', eachday, delimiter=',')
-    DayNumFileName +=1
-
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        myEnsembleData = numpy.load(f,allow_pickle=True)
+        print("successfully loaded the data into memory.")
+        for eachLocation in myEnsembleData.files:
+            DayNumFileName = 0
+            for eachDay in myEnsembleData[eachLocation]:
+                numpy.savetxt(directory +"\\AsCSV\\" + eachLocation + "\\" +  filename + eachLocation + str(DayNumFileName) + '.csv', eachDay, delimiter=',')
+                DayNumFileName +=1
+            print("finished " + eachLocation + " in " + filename)
 print("successfully saved all the data to csv's.")
 
